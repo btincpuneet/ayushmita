8f8a98e6f53e12dccfe62a005800aab3c92c13ca
Sanjeet kumar <sanjeet.kumar@bluethinkinc.com>
2025-12-11 19:46:21 +0530

    My latest changes

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -215,13 +215,12 @@ const Footer: React.FC = () => {
         <p className="mt-6 text-start sm:text-center text-gray-300 text-sm"
           style={{
             fontFamily: 'Poppins',
-            fontWeight: 400,          // Regular weight
-            fontStyle: 'normal',      // Regular corresponds to normal
+            fontWeight: 400,
+            fontStyle: 'normal',
             fontSize: '16px',
             lineHeight: '28px',
             letterSpacing: '0%',
             verticalAlign: 'middle',
-            // leading-trim: NONE is not standard CSS, so it's ignored
           }}>
           © {new Date().getFullYear()} ayushm.com. All Rights Reserved
         </p>
